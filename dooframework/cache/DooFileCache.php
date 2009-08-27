4ae9788c704f55e16b3134f3f5cda034039ba3b6
darkredz <darkredz@eaaf3d48-7251-11de-8585-3f51e31374b7>
2009-08-27 13:44:56 +0000

    bug fix on DooFileCache set/get not storing at the defined folders.
    git-svn-id: http://doophp.googlecode.com/svn/trunk@57 eaaf3d48-7251-11de-8585-3f51e31374b7

--- dooframework/cache/DooFileCache.php
+++ dooframework/cache/DooFileCache.php
@@ -21,86 +21,86 @@
 Doo::loadCore('cache/DooCache');
 
 class DooFileCache extends DooCache {
-	
-	private static $_cache;
-	
-	private $_directory;
+    
+    private static $_cache;
+    
+    private $_directory;
 
-	public function __construct($path='') {
-		if ( $path=='' ) {
-			if(isset(Doo::conf()->CACHE_PATH))
-				$this->_directory = Doo::conf()->CACHE_PATH;
-			else
-				$this->_directory = Doo::conf()->SITE_PATH . 'protected/cache/';
-		}else{
-			$this->_directory = $path;
-		}
-	}
-	
-	/**
-	 * Get the cache instance
-	 * @return DooFileCache
-	 */
-	static function cache() {
-		if (self::$_cache == null) {
-			self::$_cache = new DooFileCache();
-		}
-		return self::$_cache;
-	}
-	
-	/**
-	 * Get value from file by $id
-	 * @see dooframework/cache/DooCache#get($id)
-	 */
-	public function get($id) {
-		if (file_exists($this->generateKey($id)))
-			return unserialize(file_get_contents($this->generateKey($id)));
-		else
-			return null;
-	}
-	
-	/**
-	 * Set value into file by $id
-	 * If $duration = 0, set it to a year
-	 * @see dooframework/cache/DooCache#set($id, $value, $duration)
-	 */
-	public function set($id, $value, $duration = 0) {
-		if ($duration < 0)
-			$duration = 31536000;
-		$duration = $duration + time();
-		if (file_put_contents($this->generateKey($id), serialize($value), LOCK_EX)) {
-			chmod($this->generateKey($id),0777);
-			return touch($this->generateKey($id),$duration);
-		}
-		else
-			return false;
-	}
-	
-	/**
-	 * Delete a cache file by Id
-	 * @param $id Id of the cache
-	 * @return mixed
-	 */
-	public function flush($id) {
-		if ($id !== null) {
-			unlink($this->generateKey($id));
-			return true;
-		}
-		return false;
-	}
+    public function __construct($path='') {
+        if ( $path=='' ) {
+            if(isset(Doo::conf()->CACHE_PATH))
+                $this->_directory = Doo::conf()->CACHE_PATH;
+            else
+                $this->_directory = Doo::conf()->SITE_PATH . 'protected/cache/';
+        }else{
+            $this->_directory = $path;
+        }
+    }
+    
+    /**
+     * Get the cache instance
+     * @return DooFileCache
+     */
+    static function cache() {
+        if (self::$_cache == null) {
+            self::$_cache = new DooFileCache();
+        }
+        return self::$_cache;
+    }
+    
+    /**
+     * Get value from file by $id
+     * @see dooframework/cache/DooCache#get($id)
+     */
+    public function get($id) {
+        $cfile = $this->_directory . $this->generateKey($id);
+        if (file_exists($cfile))
+            return unserialize(file_get_contents($cfile));
+    }
+    
+    /**
+     * Set value into file by $id
+     * If $duration = 0, set it to a year
+     * @see dooframework/cache/DooCache#set($id, $value, $duration)
+     */
+    public function set($id, $value, $duration = 0) {
+        if ($duration < 0)
+            $duration = 31536000;
+        $duration = $duration + time();
+        $cfile = $this->_directory.$this->generateKey($id);
+        if (file_put_contents($cfile , serialize($value), LOCK_EX)) {
+            chmod($cfile, 0777);
+            return touch($cfile, $duration);
+        }
+        else
+            return false;
+    }
+    
+    /**
+     * Delete a cache file by Id
+     * @param $id Id of the cache
+     * @return mixed
+     */
+    public function flush($id) {
+        if ($id !== null) {
+            unlink($this->generateKey($id));
+            return true;
+        }
+        return false;
+    }
 
         /**
          * Deletes all data cache files
          * @return bool
          */
-	public function flushAll() {
-		$handle = opendir($this->_directory);
-		
-		while(($file = readdir($handle)) !== false) {
-			if (is_file($file))
+    public function flushAll() {
+        $handle = opendir($this->_directory);
+        
+        while(($file = readdir($handle)) !== false) {
+            if (is_file($file))
                             unlink($file);
-		}
-		return true;
-	}
+        }
+        return true;
+    }
 }
 ?>
