d02103bfdfd3c014796ed4f19962d5c23dc54cde
darkredz <darkredz@eaaf3d48-7251-11de-8585-3f51e31374b7>
2009-10-10 00:25:39 +0000

    App folder updated for advance modules support.
    git-svn-id: http://doophp.googlecode.com/svn/trunk@179 eaaf3d48-7251-11de-8585-3f51e31374b7

---
+++ app/protected/module/example/plugin/template_tags.php
@@ -0,0 +1,72 @@
+<?php
+
+//register functions to be used with your template files
+$template_tags = array('upper', 'tofloat', 'sample_with_args', 'debug', 'url', 'url2', 'function_deny', 'isset', 'empty');
+
+// the 1st argument must be the variable passed in from template, the other args should NOT be variables
+function upper($str){
+    return strtoupper($str);
+}
+
+function tofloat($str){
+    return sprintf("%.2f", $str);
+}
+
+function sample_with_args($str, $prefix){
+    return $str .' with args: '. $prefix;
+}
+
+function debug($var){
+    if(!empty($var)){
+        echo '<pre>';
+        print_r($var);
+        echo '</pre>';
+    }
+}
+
+//This will be called when a function NOT Registered is used in IF or ElseIF statment
+function function_deny($var=null){
+   echo '<span style="color:#ff0000;">Function denied in IF or ElseIF statement!</span>';
+   exit;
+}
+
+//Build URL based on route id
+function url($addRootUrl, $id, $param=null){
+    Doo::loadHelper('DooUrlBuilder');
+    // param pass in as string with format
+    // 'param1=>this_is_my_value, param2=>something_here'
+
+    if($param!=null){
+        $param = explode(', ', $param);
+        $param2 = null;
+        foreach($param as $p){
+            $splited = explode('=>', $p);
+            $param2[$splited[0]] = $splited[1];
+        }
+        return DooUrlBuilder::url($id, $param2, $addRootUrl);
+    }
+
+    return DooUrlBuilder::url($id, null, $addRootUrl);
+}
+
+
+//Build URL based on controller and method name
+function url2($addRootUrl, $controller, $method, $param=null){
+    Doo::loadHelper('DooUrlBuilder');
+    // param pass in as string with format
+    // 'param1=>this_is_my_value, param2=>something_here'
+
+    if($param!=null){
+        $param = explode(', ', $param);
+        $param2 = null;
+        foreach($param as $p){
+            $splited = explode('=>', $p);
+            $param2[$splited[0]] = $splited[1];
+        }
+        return DooUrlBuilder::url2($controller, $method, $param2, $addRootUrl);
+    }
+
+    return DooUrlBuilder::url2($controller, $method, null, $addRootUrl);
+}
+
+?>
