1ce2760d4968191a9b2d8af53de9d570ade6c786
zohaib.hassan@gmail.com <zohaib.hassan@gmail.com@eaaf3d48-7251-11de-8585-3f51e31374b7>
2009-11-26 10:38:29 +0000

    Introduced DooBenchmark class that allows user level benchmarking through a singleton of DooBenchmark.
    git-svn-id: http://doophp.googlecode.com/svn/trunk@229 eaaf3d48-7251-11de-8585-3f51e31374b7

---
+++ dooframework/helper/DooBenchmark.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * DooBechmark class file.
+ * @package doo.helper
+ * @author Zohaib Sibt-e-Hassan <zohaib.hassan@gmail.com>
+ * @link http://www.doophp.com/
+ * @copyright Copyright &copy; 2009 Zohaib Sibt-e-Hassan
+ * @license http://www.doophp.com/license
+ */
+
+/**
+* DooBenchmark class, for benchmarking memory and time of particular code segment
+*
+* @author Zohaib Sibt-e-Hassan <zohaib.hassan@gmail.com>
+* @copyright &copy; 2009 Zohaib Sibt-e-Hassan
+* @license http://www.doophp.com/license
+*/
+
+class DooBenchmark {
+    /**
+    * Associative array of marked label as key and value of microtime
+    * 
+    * @var array
+    */
+    protected static $timeArr = array();
+    
+    /**
+    * Associative array of marked label as key and value of memory used
+    * 
+    * @var array
+    */
+    protected static $memoArr = array();
+    
+    /**
+    * Associative array of saved points that can be fetched later on and displayed
+    * Keys are mark names and values are objects carrying the 
+    * @var array
+    */
+    
+    protected static $savedPoints = array();
+    
+    /**
+    * Mark current position with given name
+    * 
+    * @param string $name
+    */
+    
+    public static function mark($name){
+        DooBenchmark::$timeArr[$name] = microtime();
+        DooBenchmark::$memoArr[$name] = memory_get_usage();
+    }
+    
+    /**
+    * Finish the bechmarking process and return the total memory
+    * and time consumed as obj->time and obj->memory
+    * 
+    * @param string $name
+    * @param int $decimals
+    * @return Object (time and memory containing elapsed values)
+    */
+    public static function get($name){
+        list($startMic, $startSec) = explode(' ', DooBenchmark::$timeArr[$name]);
+        list($endMic, $endSec) = explode(' ', microtime());
+        $tm = (float)($endMic + $endSec) - (float)($startMic + $startSec);
+        
+        $total = memory_get_usage() - DooBenchmark::$memoArr[$name];
+        $mem = $total;
+        
+        $ret=new DooBenchmark();
+        $ret->memory = $mem;
+        $ret->time = $tm;
+        
+        return $ret;
+    }
+    
+    /**
+    * Save all the unsaved benchmarks
+    * This subroutine will look up for any benchmarks that were marked with DooBenchmark::mark
+    * And for all unsaved 
+    * 
+    * @param int $decimals number of decimal precisions as place
+    */
+    
+    public static function saveAllMarks(){
+        list($em, $es) = explode(' ', microtime());
+        $mmus = memory_get_usage();
+        
+        foreach(DooBenchmark::$memoArr as $k => &$val){
+            if( !isset(DooBenchmark::$savedPoints[$k]) ){
+                list($sm, $ss) = explode(' ', DooBenchmark::$timeArr[$k]);
+                $tm = (float)($em + $es) - (float)($sm + $ss);
+                
+                
+                $total =  $mmus - DooBenchmark::$memoArr[$k];
+                $mem = $total;
+                
+                $ret=new DooBenchmark();
+                $ret->memory = $mem;
+                $ret->time = $tm;
+                
+                DooBenchmark::$savedPoints[$k] = $ret;
+                
+            }
+        }
+    }
+    
+    /**
+    * Save current bechmark associateed with its name
+    * 
+    * @param string $name name of bencmark item marked with mark function previously
+    */
+    
+    public static function saveMark($name){
+        DooBenchmark::$savedPoints[$name] = DooBenchmark::get($name, $decimal);
+    }
+    
+    /**
+    * Get all the saved points
+    * 
+    */
+    
+    public static function getAll(){
+        return DooBenchmark::$savedPoints;
+    }
+}
+
+?>
