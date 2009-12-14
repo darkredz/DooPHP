7af78ddf8b94dbca3b13b70fbf99255e8f1116db
darkredz <darkredz@eaaf3d48-7251-11de-8585-3f51e31374b7>
2009-12-14 06:57:30 +0000

    added clientIP()
    git-svn-id: http://doophp.googlecode.com/svn/trunk@267 eaaf3d48-7251-11de-8585-3f51e31374b7

--- dooframework/controller/DooController.php
+++ dooframework/controller/DooController.php
@@ -292,7 +292,22 @@ class DooController {
         if(isset($extensions[$type]))
             header("Content-Type: {$extensions[$type]}; charset=$charset");
     }
-
+	
+    /**
+     * Get client's IP
+     * @return string
+     */
+	public function clientIP(){
+		if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+		   return getenv('HTTP_CLIENT_IP');
+		} elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+		   return getenv('HTTP_X_FORWARDED_FOR');
+		} elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+		   return getenv('REMOTE_ADDR');
+		} elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+		   return $_SERVER['REMOTE_ADDR'];
+		}
+	}
     /**
      * Check if the request is an AJAX request usually sent with JS library such as JQuery/YUI/MooTools
      * @return bool
