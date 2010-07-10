dfcec31e26611203f14bf563b279da2ac401f74f
darkredz@gmail.com <darkredz@gmail.com@eaaf3d48-7251-11de-8585-3f51e31374b7>
2010-07-10 14:48:54 +0000

    removed repeated line on highlightPHP
    git-svn-id: http://doophp.googlecode.com/svn/trunk@444 eaaf3d48-7251-11de-8585-3f51e31374b7

--- dooframework/helper/DooTextHelper.php
+++ dooframework/helper/DooTextHelper.php
@@ -195,7 +195,7 @@ class DooTextHelper {
             $str = preg_replace('#color="(.*?)"#', 'style="color: \\1"', $str);
         }
         $str = preg_replace("#\<code\>.+?//tempstart\<br />\</span\>#is", "<code>\n", $str);
-        $str = preg_replace("#\<code\>.+?//tempstart\<br />#is", "<code>\n", $str);
+        //$str = preg_replace("#\<code\>.+?//tempstart\<br />#is", "<code>\n", $str);
         $str = preg_replace("#//tempend.+#is", "</span>\n</code>", $str);
         $str = str_replace(array('phptagopen', 'phptagclose', 'backslashtmp'), array('&lt;?php', '?&gt;', '\\'), $str);
         return $str;
