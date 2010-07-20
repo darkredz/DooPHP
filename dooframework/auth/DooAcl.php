fe0cc0abbb80bc2b9b53136b728b2cd4a1ff85c5
richard.myers@hotmail.co.uk <richard.myers@hotmail.co.uk@eaaf3d48-7251-11de-8585-3f51e31374b7>
2010-07-20 16:27:31 +0000

    Changed the DooAcl and DooRbAcl classes as per: http://doophp.com/forum/viewtopic.php?f=9&t=1260 Note: Not properly tested! (at least by myself)
    git-svn-id: http://doophp.googlecode.com/svn/trunk@452 eaaf3d48-7251-11de-8585-3f51e31374b7

--- dooframework/auth/DooAcl.php
+++ dooframework/auth/DooAcl.php
@@ -12,52 +12,52 @@
  * Provides Access Control List feature to the application.
  *
  * <p>DooAcl performs authorization checks for the specified resource and action. It checks against the rules defined in acl.conf.php.</p>
- * <p>Only when the user is allowed by one of the rules, will he be able to access the action. 
+ * <p>Only when the user is allowed by one of the rules, will he be able to access the action.
  * If the user role cannot be found in both deny and allow list, he will not be able to access the action/resource</p>
  *
  * <p>Rules has to be defined in this way:</p>
  * <code>
  * # Allow member to access all actions in Sns and Blog resource.
  * $acl['member']['allow'] = array(
- * 				'SnsController'=>'*', 
- * 				'BlogController'=>'*', 
- * 			);
- * 
+ *             'SnsController'=>'*',
+ *             'BlogController'=>'*',
+ *          );
+ *
  * # Allow anonymous visitors for Blog index only.
  * $acl['anonymous']['allow'] = array(
- * 				'BlogController'=>'index', 
- * 			);
+ *             'BlogController'=>'index',
+ *          );
  *
  * # Deny member from banUser, showVipHome, etc.
  * $acl['member']['deny'] = array(
- * 				'SnsController'=>array('banUser', 'showVipHome'), 
- * 				'BlogController' =>array('deleteComment', 'writePost')
- * 			);
+ *             'SnsController'=>array('banUser', 'showVipHome'),
+ *             'BlogController' =>array('deleteComment', 'writePost')
+ *          );
  *
  * # Admin can access all except Sns showVipHome
  * $acl['admin']['allow'] = '*';
  * $acl['admin']['deny'] = array(
- * 				'SnsController'=>array('showVipHome')
- * 			);
+ *             'SnsController'=>array('showVipHome')
+ *          );
  *
  * # If member is denied, reroute to the following routes.
  * $acl['member']['failRoute'] = array(
- * 				//if not found this will be used
- * 				'_default'=>'/error/member',	
- * 				
- * 				//if denied from sns banUser
- * 				'SnsController/banUser'=>'/error/member/sns/notAdmin', 
- * 				
- * 				'SnsController/showVipHome'=>'/error/member/sns/notVip', 
- * 				'BlogController'=>'/error/member/blog/notAdmin' 
- * 			);
+ *             //if not found this will be used
+ *             '_default'=>'/error/member',
+ *
+ *             //if denied from sns banUser
+ *             'SnsController/banUser'=>'/error/member/sns/notAdmin',
+ *
+ *             'SnsController/showVipHome'=>'/error/member/sns/notVip',
+ *             'BlogController'=>'/error/member/blog/notAdmin'
+ *          );
  * </code>
  *
  * <p>You have to assign the rules to DooAcl in bootstrap.</p>
  * <code>
  * # set rules
  * Doo::acl()->rules = $acl;
- * 
+ *
  * # The default route to be reroute to when resource is denied. If not set, 404 error will be displayed.
  * Doo::acl()->defaultFailedRoute = '/error';
  * </code>
@@ -67,147 +67,170 @@
  * @package doo.auth
  * @since 1.1
  */
-class DooAcl{
-    /**
-     * Rules settings for the application ACL. Defined in acl.conf.php
-     * @var array
-     */
-    public $rules;
+class DooAcl {
+	/**
+	 * Rules settings for the application ACL. Defined in acl.conf.php
+	 * @var array
+	 */
+	public $rules;
 
-    /**
-     * Default route to be reroute to if no custome fail route is defined for a certain rule.
-     * @var string|array
-     */
-    public $defaultFailedRoute = array('/error-default/failed-route/please-set-in-route', 404);
+	/**
+	 * Default route to be reroute to if no custome fail route is defined for a certain rule.
+	 * @var string|array
+	 */
+	public $defaultFailedRoute = array('/error-default/failed-route/please-set-in-route', 404);
 
-    /**
-     * Check if the user Role is allowed to access the resource or action list or both.
-     *
-     * <code>
-     * //Check if member is allowed for BlogController->post
-     * Doo::acl()->isAllowed('member', 'BlogController', 'post' );
-     *
-     * //Check if member is allowed for BlogController
-     * Doo::acl()->isAllowed('member', 'blog');
-     * </code>
-     * 
-     * @param string $role Role of a user, usually retrieve from user's login session
-     * @param string $resource Resource name (use Controller class name)
-     * @param string $action Action name (use Method name)
-     * @return bool
-     */
-    public function isAllowed($role, $resource, $action=''){
-        if($action==''){
-            return isset($this->rules[$role]['allow'][$resource]);
-        }
-        else{
-            if(isset($this->rules[$role]['allow'][$resource])){
-                $actionlist = $this->rules[$role]['allow'][$resource];
+	/**
+	 * Check if the user Role is in the allowed rule
+	 *
+	 * @param string $role Role of a user, usually retrieve from user's login session
+	 * @param string $resource Resource name (use Controller class name)
+	 * @param string $action Action name (use Method name)
+	 * @return bool
+	 */
+	protected function hasAllowed($role, $resource, $action='') {
+		if ($action=='') {
+			return isset($this->rules[$role]['allow'][$resource]);
+		} else {
+			if(isset($this->rules[$role]['allow'][$resource])) {
+				$actionlist = $this->rules[$role]['allow'][$resource];
 
-                if($actionlist==='*')
-                    return true;
-                else
-                    return in_array($action, $actionlist);
-            }
-            else
-                return false;
-        }
-    }
+				if ($actionlist==='*')
+					return true;
+				else
+					return in_array($action, $actionlist);
+			} else {
+				return false;
+			}
+		}
+	}
 
-    /**
-     * Check if the user Role is denied from the resource or action list or both.
-     *
-     * <code>
-     * //Check if member is denied from BlogController->post
-     * Doo::acl()->isDenied('member', 'BlogController', 'post' );
-     *
-     * //Check if member is denied from BlogController
-     * Doo::acl()->isDenied('member', 'blog');
-     * </code>
-     *
-     * @param string $role Role of a user, usually retrieve from user's login session
-     * @param string $resource Resource name (use Controller class name)
-     * @param string $action Action name (use Method name)
-     * @return bool
-     */
-    public function isDenied($role, $resource, $action=''){
-        if($action==''){
-            return isset($this->rules[$role]['deny'][$resource]);
-        }
-        else{
-            if(isset($this->rules[$role]['deny'][$resource])){
-                $actionlist = $this->rules[$role]['deny'][$resource];
+	/**
+	 * Check if the user Role is allowed to access the resource or action list or both.
+	 *
+	 * <code>
+	 * //Check if member is allowed for BlogController->post
+	 * Doo::acl()->isAllowed('member', 'BlogController', 'post' );
+	 *
+	 * //Check if member is allowed for BlogController
+	 * Doo::acl()->isAllowed('member', 'BlogController');
+	 * </code>
+	 *
+	 * @param string $role Role of a user, usually retrieve from user's login session
+	 * @param string $resource Resource name (use Controller class name)
+	 * @param string $action Action name (use Method name)
+	 * @return bool
+	 */
+	public function isAllowed($role, $resource, $action='') {
+		if (!$this->hasDenied($role, $resource, $action)) {
+			if ($this->hasAllowed($role, $resource, $action)) {
+				return true;
+			}
+		}
 
-                if($actionlist==='*')
-                    return true;
-                else
-                    return in_array($action, $actionlist);
-            }
-            else
-                return false;
-        }
-    }
+		return false;
+	}
 
-    /**
-     * Check if the user's role is able to access the resource/action.
-     * 
-     * @param string $role Role of a user, usually retrieve from user's login session
-     * @param string $resource Resource name (use Controller class name)
-     * @param string $action Action name (use Method name)
-     * @return array|string Returns the fail route if user cannot access the resource.
-     */
-    public function process($role, $resource, $action=''){
-        if( $this->isDenied($role, $resource, $action) ){
-            //echo 'In deny list';
+	/**
+	 * Check if the user Role is denied from the resource or action list or both.
+	 *
+	 * @param string $role Role of a user, usually retrieve from user's login session
+	 * @param string $resource Resource name (use Controller class name)
+	 * @param string $action Action name (use Method name)
+	 * @return bool
+	 */
+	protected function hasDenied($role, $resource, $action='') {
+		if ($action=='') {
+			return isset($this->rules[$role]['deny'][$resource]);
+		} else {
+			if (isset($this->rules[$role]['deny'][$resource])) {
+				$actionlist = $this->rules[$role]['deny'][$resource];
 
-            if(isset($this->rules[$role]['failRoute'])){
-                $route = $this->rules[$role]['failRoute'];
+				if($actionlist==='*')
+					return true;
+				else
+					return in_array($action, $actionlist);
+			} else {
+				return false;
+			}
+		}
+	}
 
-                if(is_string($route)){
-                    return array($route, 'internal');
-                }
-                else{
-                    if(isset($route[$resource])){
-                        return (is_string($route[$resource]))? array($route[$resource], 'internal') : $route[$resource] ;
-                    }
-                    else if(isset( $route[$resource.'/'.$action] )){
-                        $rs = $route[$resource.'/'.$action];
-                        return (is_string($rs))? array($rs, 'internal') : $rs;
-                    }
-                    else if(isset( $route['_default'] )){
-                        return (is_string($route['_default']))? array($route['_default'], 'internal') : $route['_default'];
-                    }
-                    else{
-                        return (is_string($this->defaultFailedRoute))? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
-                    }
-                }
-            }
-        }
-        else if($this->isAllowed($role, $resource, $action)==false){
-            //echo 'Not in allow list<br>';
+	/**
+	 * Check if the user Role is denied from the resource or action list or both.
+	 *
+	 * <code>
+	 * //Check if member is denied from BlogController->post
+	 * Doo::acl()->isDenied('member', 'BlogController', 'post' );
+	 *
+	 * //Check if member is denied from BlogController
+	 * Doo::acl()->isDenied('member', 'BlogController');
+	 * </code>
+	 *
+	 * @param string $role Role of a user, usually retrieve from user's login session
+	 * @param string $resource Resource name (use Controller class name)
+	 * @param string $action Action name (use Method name)
+	 * @return bool
+	 */
+	public function isDenied($role, $resource, $action='') {
+		if ($this->hasDenied($role, $resource, $action)) {
+			return true;
+		}
 
-            if(isset($this->rules[$role]['failRoute'])){
-                $route = $this->rules[$role]['failRoute'];
+		return false;
+	}
 
-                if(is_string($route)){
-                        return array($route, 'internal');
-                }
-                else{
-                    if(isset($route[$resource])){
-                        return (is_string($route[$resource]))? array($route[$resource], 'internal') : $route[$resource] ;
-                    }
-                    else if(isset( $route[$resource.'/'.$action] )){
-                        $rs = $route[$resource.'/'.$action];
-                        return (is_string($rs))? array($rs, 'internal') : $rs;
-                    }
-                    else if(isset( $route['_default'] )){
-                        return (is_string($route['_default']))? array($route['_default'], 'internal') : $route['_default'];
-                    }
-                    else{
-                        return (is_string($this->defaultFailedRoute))? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
-                    }
-                }
-            }
-        }
-    }
+	/**
+	 * Check if the user's role is able to access the resource/action.
+	 *
+	 * @param string $role Role of a user, usually retrieve from user's login session
+	 * @param string $resource Resource name (use Controller class name)
+	 * @param string $action Action name (use Method name)
+	 * @return array|string Returns the fail route if user cannot access the resource.
+	 */
+	public function process($role, $resource, $action='') {
+		if ($this->isDenied($role, $resource, $action) ) {
+			//echo 'In deny list';
+
+			if (isset($this->rules[$role]['failRoute'])) {
+				$route = $this->rules[$role]['failRoute'];
+
+				if (is_string($route)) {
+					return array($route, 'internal');
+				} else {
+					if (isset($route[$resource])) {
+						return (is_string($route[$resource]))? array($route[$resource], 'internal') : $route[$resource] ;
+					} elseif (isset( $route[$resource.'/'.$action] )) {
+						$rs = $route[$resource.'/'.$action];
+						return (is_string($rs))? array($rs, 'internal') : $rs;
+					} elseif (isset( $route['_default'] )) {
+						return (is_string($route['_default']))? array($route['_default'], 'internal') : $route['_default'];
+					} else {
+						return (is_string($this->defaultFailedRoute))? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
+					}
+				}
+			}
+		} else if($this->isAllowed($role, $resource, $action)==false) {
+			//echo 'Not in allow list<br>';
+
+			if (isset($this->rules[$role]['failRoute'])) {
+				$route = $this->rules[$role]['failRoute'];
+
+				if (is_string($route)) {
+					return array($route, 'internal');
+				} else {
+					if (isset($route[$resource])) {
+						return (is_string($route[$resource]))? array($route[$resource], 'internal') : $route[$resource] ;
+					} elseif (isset( $route[$resource.'/'.$action] )) {
+						$rs = $route[$resource.'/'.$action];
+						return (is_string($rs))? array($rs, 'internal') : $rs;
+					} elseif (isset( $route['_default'] )) {
+						return (is_string($route['_default']))? array($route['_default'], 'internal') : $route['_default'];
+					} else {
+						return (is_string($this->defaultFailedRoute))? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
+					}
+				}
+			}
+		}
+	}
 }
--- dooframework/auth/DooRbAcl.php
+++ dooframework/auth/DooRbAcl.php
@@ -13,6 +13,68 @@ Doo::loadCore('auth/DooAcl');
 class DooRbAcl extends DooAcl{
 
     /**
+     * Check if the user Roles is allowed to access the resource or action list or both.
+     *
+     * <code>
+     * //Check if members are allowed for BlogController->post
+     * Doo::acl()->isAllowed(array('member','admin'), 'BlogController', 'post' );
+     *
+     * //Check if members are allowed for BlogController
+     * Doo::acl()->isAllowed(array('anonymous','member'), 'BlogController');
+     * </code>
+     *
+     * @param array $role Roles of a user, usually retrieve from user's login session
+     * @param string $resource Resource name (use Controller class name)
+     * @param string $action Action name (use Method name)
+     * @return bool
+     */
+    public function isAllowed($roles, $resource, $action='') {
+       $allowed=false;
+        foreach($roles as $role){
+           if ($this->hasDenied($role, $resource, $action)) {
+              return false;
+           }
+
+           if ($this->hasAllowed($role, $resource, $action)) {
+              $allowed=true;
+           }
+        }
+
+      return $allowed;
+    }
+
+    /**
+     * Check if the user Roles is denied from the resource or action list or both.
+     *
+     * <code>
+     * //Check if members are denied from BlogController->post
+     * Doo::acl()->isDenied(array('member','admin'), 'BlogController', 'post' );
+     *
+     * //Check if members are denied from BlogController
+     * Doo::acl()->isDenied(array('anonymous','member'), 'BlogController');
+     * </code>
+     *
+     * @param string $role Role of a user, usually retrieve from user's login session
+     * @param string $resource Resource name (use Controller class name)
+     * @param string $action Action name (use Method name)
+     * @return bool
+     */
+    public function isDenied($roles, $resource, $action='') {
+       $denied=true;
+        foreach($roles as $role){
+           if ($this->hasDenied($role, $resource, $action)) {
+              return true;
+           }
+
+           if ($this->hasAllowed($role, $resource, $action)) {
+              $denied=false;
+           }
+        }
+
+        return $denied;
+    }
+
+    /**
      * Check if the user's role is able to access the resource/action.
      *
      * @param mixed $roles Roles of a user, usually retrieve from user's login session
@@ -22,38 +84,38 @@ class DooRbAcl extends DooAcl{
      */
     public function process($roles, $resource, $action=''){
 
-		if(!is_array($roles)){
-			$roles = explode(',', $roles);
-		}
-		
-		$denied = false;
-		$allowed = false;
-		foreach($roles as $role) {
-			$denied = $denied || $this->isDenied($role, $resource, $action);
-			$allowed = $allowed || $this->isAllowed($role, $resource, $action);
-		}
+      if (!is_array($roles)){
+         $roles = explode(',', $roles);
+      }
 
-		if( $denied ){
+      $denied = false;
+      $allowed = false;
+      foreach($roles as $role) {
+         $denied = $denied || $this->hasDenied($role, $resource, $action);
+         $allowed = $allowed || $this->hasAllowed($role, $resource, $action);
+      }
+
+      if( $denied ){
             //echo 'In deny list';
-            if(isset($this->rules[$role]['failRoute'])){
+            if (isset($this->rules[$role]['failRoute'])) {
                 $route = $this->rules[$role]['failRoute'];
 
-                if(is_string($route)){
+                if (is_string($route)){
                     return array($route, 'internal');
-                } else{
-                    if(isset($route[$resource])) {
+                } else {
+                    if (isset($route[$resource])) {
                         return (is_string($route[$resource])) ? array($route[$resource], 'internal') : $route[$resource] ;
-                    } else if(isset( $route[$resource.'/'.$action] )){
+                    } else if (isset( $route[$resource.'/'.$action] )) {
                         $rs = $route[$resource.'/'.$action];
                         return (is_string($rs))? array($rs, 'internal') : $rs;
-                    } else if(isset( $route['_default'] )){
+                    } else if (isset( $route['_default'] )) {
                         return (is_string($route['_default'])) ? array($route['_default'], 'internal') : $route['_default'];
                     }
                 }
             }
-			return (is_string($this->defaultFailedRoute)) ? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
-			
-        } else if($allowed==false){
+         return (is_string($this->defaultFailedRoute)) ? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
+
+        } else if ($allowed==false) {
             //echo 'Not in allow list<br>';
             if(isset($this->rules[$role]['failRoute'])){
                 $route = $this->rules[$role]['failRoute'];
@@ -61,18 +123,17 @@ class DooRbAcl extends DooAcl{
                 if(is_string($route)){
                         return array($route, 'internal');
                 } else{
-                    if(isset($route[$resource])){
+                    if( isset($route[$resource])) {
                         return (is_string($route[$resource])) ? array($route[$resource], 'internal') : $route[$resource] ;
-                    } else if(isset( $route[$resource.'/'.$action] )){
+                    } else if (isset( $route[$resource.'/'.$action] )) {
                         $rs = $route[$resource.'/'.$action];
                         return (is_string($rs))? array($rs, 'internal') : $rs;
-                    } else if(isset( $route['_default'] )){
+                    } else if (isset( $route['_default'] )) {
                         return (is_string($route['_default'])) ? array($route['_default'], 'internal') : $route['_default'];
                     }
                 }
             }
-			return (is_string($this->defaultFailedRoute)) ? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
+         return (is_string($this->defaultFailedRoute)) ? array($this->defaultFailedRoute, 404) : $this->defaultFailedRoute;
         }
     }
 }
-
